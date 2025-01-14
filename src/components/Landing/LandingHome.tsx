ef38f8e8136334eed4c27e3eb701368c5209a7f2
Saim Shahzad <saimshehzad2030@gmail.com>
2025-01-14 15:44:50 +0500

    few changes

--- src/components/Landing/LandingHome.tsx
+++ src/components/Landing/LandingHome.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const LandingHome = () => {
   return (
     <div className="fixed   left-0 w-full sm:h-[100vh] h-[50vh]    flex flex-col items-center justify-center   z-0  ">
-      <div className="flex flex-row items-center justify-evenly  px-8 lg:px-10 xl:px-20   h-auto overflow-hidden  pt-8 ">
+      <div className="flex flex-row items-center justify-evenly lg:justify-between  px-8  xl:px-20   h-auto overflow-hidden  pt-8  ">
         <div
           className="flex flex-col items-start w-7/12 sm:w-6/12"
           data-aos="fade-left"
