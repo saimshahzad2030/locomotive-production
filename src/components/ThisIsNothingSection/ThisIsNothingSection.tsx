da44a30cc1db410f8c9980efc1c1cc35fd12e2c6
Saim Shahzad <saimshehzad2030@gmail.com>
2025-01-29 17:35:36 +0500

    few chganes

--- src/components/ThisIsNothingSection/ThisIsNothingSection.tsx
+++ src/components/ThisIsNothingSection/ThisIsNothingSection.tsx
@@ -30,7 +30,7 @@ const ThisIsNothingSection = () => {
         <div className="relative w-full flex flex-row items-center justify-end">
           <motion.button
             onClick={() => setIsOpen(!isOpen)}
-            className="absolute right-[-100] bg-black bg-opacity-20  backdrop-blur-md border border-white/20 text-white h-12 w-24 rounded-full  flex flex-row items-center justify-start pl-4"
+            className="absolute right-[0px] bg-black bg-opacity-20  backdrop-blur-md border border-white/20 text-white h-12 w-12 rounded-l-full  flex flex-row items-center justify-start pl-4"
             whileHover={{ scale: 1.1 }}
           >
             <motion.span
@@ -46,16 +46,16 @@ const ThisIsNothingSection = () => {
                 <path
                   d="M19 19L12.7071 12.7071C12.3166 12.3166 12.3166 11.6834 12.7071 11.2929L19 5"
                   stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
                 <path
                   d="M11 19L4.70711 12.7071C4.31658 12.3166 4.31658 11.6834 4.70711 11.2929L11 5"
                   stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
             </motion.span>
