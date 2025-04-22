a5f68d2c012ac04ddae302bae245e0371c7858f5
Saim Shahzad <saimshehzad2030@gmail.com>
2025-04-22 16:36:08 +0500

    few changes

--- src/components/AboutSecondSection/AboutSecondSection.tsx
+++ src/components/AboutSecondSection/AboutSecondSection.tsx
@@ -7,11 +7,11 @@ const AboutSecondSection = () => {
   return (
     <div
       style={{ opacity: 1 }}
-      className="  flex flex-col items-center w-full overflow-x-hidden overflow-y-hidden o "
+      className={` flex flex-col items-center w-full overflow-x-hidden overflow-y-hidden  ${playfairDisplay.className} `}
     >
       <div
         data-aos="fade-up"
-        className="  bg-opacity-100 opac text-black z-20   overflow-y-hidden overflow-x-hidden  flex flex-col items-center justify-center   absolute top-[70vh] sm:top-[100vh]"
+        className="  bg-opacity-100 opac text-white z-20   overflow-y-hidden overflow-x-hidden  flex flex-col items-center justify-center   absolute top-[70vh] sm:top-[100vh]"
       >
         <div className="absolute top-0 left-0 w-full h-64 bg-gradient-to-b from-black/10  to-[#D9A433] z-10 pointer-events-none" />
 
@@ -19,12 +19,11 @@ const AboutSecondSection = () => {
           className=" mt-64
           bg-opacity-100 opacity-100
                        bg-gradient-to-b from-[#D9A433] to-[#FFBF35]
-
            px-8 lg:px-20 xl:px-24
           w-full sm:min-h-[40vh] lg:min-h-[90vh] flex flex-col lg:flex-row items-center justify-between  "
         >
           <div
-            className="  w-full lg:w-6/12 flex flex-col items-center text-black text-center    lg:min-h-[50vh] justify-between"
+            className="  w-full lg:w-7/12 flex flex-col items-center text-white text-center    lg:min-h-[50vh] justify-between"
             data-aos="fade-right"
           >
             <h1
@@ -57,17 +56,36 @@ const AboutSecondSection = () => {
               can collaborate and push bound.
             </p>
           </div>
-          <div className="  mt-6 lg:mt-0 w-full lg:w-5/12 flex flex-col items-end sm:min-h-[40vh] lg:min-h-[80vh] justify-between">
+          <div
+            className=" 
+lg:mb-0 md:mb-12 mb-4
+          
+          mt-6 lg:mt-0   w-full lg:w-4/12 flex flex-col items-end sm:min-h-[40vh] lg:min-h-[80vh] justify-between"
+          >
             <div className="  flex flex-col items-center w-full">
-              <p
-                className={`mb-2 text-[5vw] md:text-[6vw] text-black lg:text-[2vw] ${playfairDisplay.className}`}
-              >
-                Founder of Locomotive Films
-              </p>
               <div
-                className="border border-gray-500 relative w-11/12 lg:w-11/12 min-h-[60vh] sm:min-h-[50vh] lg:min-h-[70vh]  bg-black  flex flex-col items-center justify-center"
+                className="border border-gray-500 relative w-11/12 lg:w-11/12 min-h-[60vh] sm:min-h-[70vh] lg:min-h-[85vh]  bg-black  flex flex-col items-center justify-center"
                 data-aos="fade-left"
               >
+                <span className="z-[45] uppercase tracking-[20px] sm:tracking-normal absolute left-[-14%] sm:left-[-10%] lg:left-[-32%] top-1/2 transform -translate-y-1/2 text-white text-[11vw] sm:text-[8vw] 2xl:text-[9.5vw] font-semibold whitespace-nowrap [writing-mode:vertical-rl] rotate-180">
+                  <span className="text-transparent">F</span>
+                  <span>O</span>
+                  <span className="text-transparent">U</span>
+                  <span className="text-transparent">N</span>
+                  <span className="text-transparent">D</span>
+                  <span className="text-transparent">E</span>
+                  <span className="text-transparent">R</span>
+                </span>
+                <span className="z-10 uppercase tracking-[20px] sm:tracking-normal absolute left-[-14%] sm:left-[-10%] lg:left-[-32%] top-1/2 transform -translate-y-1/2 text-white text-[11vw] sm:text-[8vw] 2xl:text-[9.5vw] font-semibold whitespace-nowrap [writing-mode:vertical-rl] rotate-180">
+                  <span>F</span>
+                  <span>O</span>
+                  <span>U</span>
+                  <span>N</span>
+                  <span>D</span>
+                  <span>E</span>
+                  <span>R</span>
+                </span>
+
                 <img
                   src="/assets/About-landing-1.png"
                   alt="about 2 image"
@@ -78,34 +96,114 @@ const AboutSecondSection = () => {
                   alt="about image"
                   className="w-full  h-full  object-cover transition-opacity duration-300 absolute     hover:opacity-0  opacity-100 z-40"
                 />
+                <h1 className="absolute bottom-[8%] sm:bottom-[5%] left-[10%] sm:left-[2%] lg:left-[8%] 2xl:left-[8.5%] z-[41] text-[12vw] sm:text-[7.1vw] 2xl:text-[10vw]">
+                  F
+                </h1>
+                <p className="font-normal w-6/12 absolute bottom-[9%] left-[22%] sm:left-[8%] lg:left-[27%] 2xl:left-[29%] leading-[7.5vw] z-[41] text-[5vw] sm:text-[2vw] 2xl:text-[3vw] 2xl:leading-[5.2vw] capitalize sm:leading-[3.5vw] ">
+                  {` Locomotive 
+                   Films`}
+                </p>
               </div>
-              {/* <img
-                className="w-11/12 lg:w-4/6 min-h-[60vh] sm:min-h-[40vh] lg:min-h-[80vh] object-cover"
-                src="/assets/About-Section2-img.png"
-              /> */}
             </div>
           </div>
         </div>
-
+        <div className="  text-white text-center  bg-gradient-to-b   from-[#FFBF35]   to-[#875d00]   pt-8  flex flex-col items-center w-full lg:py-8  py-4  px-8 lg:px-16 xl:px-20">
+          <div className=" lg:h-[100vh] flex flex-col lg:flex-row items-center justify-between w-full">
+            <div
+              className="flex flex-col-reverse lg:flex-col items-center w-full  lg:w-8/12"
+              data-aos="fade-right"
+            >
+              <div className="border border-gray-500 relative w-full lg:w-11/12   lg:h-[70vh]  bg-black  flex flex-col items-center justify-center">
+                <img
+                  src="/assets/About-Section5-img.png"
+                  alt="about image"
+                  className="w-full  h-full  object-cover transition-opacity opacity-100 duration-300    z-30"
+                />
+                <img
+                  src="/assets/About-Section5-img-bw.png"
+                  alt="about image"
+                  className="w-full  h-full  object-cover transition-opacity duration-300 absolute     hover:opacity-0  opacity-100 z-40"
+                />
+                {/* <span className="z-10   absolute left-0  top-[-25%]   text-white text-[8vw] font-semibold whitespace-nowrap  ">
+                  Executive
+                </span> */}
+                <span className="z-[45]   absolute left-0 top-[-20%] lg:top-[-25%]  xl:top-[-22%] 2xl:top-[-18%]  text-white text-[8vw] font-normal whitespace-nowrap  ">
+                  <span>E</span>
+                  <span className="text-transparent">x</span>
+                  <span className=" ">e</span>
+                  <span className="text-transparent">c</span>
+                  <span>u</span>
+                  <span className="text-transparent">t</span>
+                  <span>i</span>
+                  <span className="text-transparent">v</span>
+                  <span>e</span>
+                </span>
+                <span className="z-10   absolute left-0 top-[-20%] lg:top-[-25%] xl:top-[-22%] 2xl:top-[-18%]   text-white text-[8vw] font-normal whitespace-nowrap  ">
+                  <span>E</span>
+                  <span>x</span>
+                  <span>e</span>
+                  <span>c</span>
+                  <span>u</span>
+                  <span>t</span>
+                  <span>i</span>
+                  <span>v</span>
+                  <span>e</span>
+                </span>
+                <span className="z-[45]   absolute right-[2%]  bottom-[-6%]   text-white text-[4vw] font-normal whitespace-nowrap  ">
+                  Producer
+                </span>
+              </div>
+              {/* <img
+                src="/assets/About-Section5-img.png"
+                className="w-full lg:w-11/12  lg:h-[80vh] object-cover"
+                alt="about 3rd image"
+              /> */}
+            </div>
+            <div
+              className={`w-full lg:w-4/12 lg:h-[75vh] flex flex-col items-center justify-center `}
+              data-aos="fade-left"
+            >
+              <p
+                className={`lg:my-0 my-12 md:[word-spacing:0.2rem] lg:[word-spacing:0.5rem] lg:w-11/12 mb-2 md:mb-4 lg:mb-8 font-light  ${playfairDisplay.className} text-[2.5vw] md:text-[2vw] lg:text-[1.2vw] text-center`}
+              >
+                Mariam Al Kaabi, our Executive Producer, plays a pivotal role in
+                bringing Locomotive’s vision to life. As a key investor, her
+                support and dedication to elevating Middle Eastern cinema have
+                been instrumental in our journey.
+              </p>
+              <p
+                className={` lg:w-11/12 md:[word-spacing:0.2rem] lg:[word-spacing:0.5rem] font-light ${playfairDisplay.className} text-[2.5vw]  md:text-[2vw] lg:text-[1.2vw] text-center`}
+              >
+                Mariam’s belief in the power of storytelling and her commitment
+                to quality are integral to ensuring that every project we
+                undertake reaches its full potential.
+              </p>
+            </div>
+          </div>
+        </div>
         <div
           className="  
-                   bg-gradient-to-b   from-[#FFBF35]   to-[#875d00] 
- 
+                  
+ bg-gradient-to-b from-[#875d00] to-black
         
         pt-8  flex flex-col items-center w-full lg:py-8    px-8 lg:px-20 xl:px-24"
         >
           <div className=" lg:h-[100vh] flex flex-col lg:flex-row items-center justify-between w-full">
             <div
-              className="  flex flex-col items-center w-full  lg:w-6/12 "
+              className="  flex flex-col items-start w-full  lg:w-8/12 "
               data-aos="fade-right"
             >
-              <p
-                className={`mb-2 text-[4vw] md:text-[6vw] text-black text-center lg:text-[1.2vw] ${playfairDisplay.className}`}
+              <h1
+                className={`uppercase text-[3vw] lg:text-[4vw] tracking-[0.8vw] font-normal`}
               >
-                Our films are not just meant to be watched—they’re meant to be
-                lived
+                Our films
+              </h1>
+              <p
+                className={`text-justifYy text-[2vw] md:text-[3vw] text-white w-full lg:text-[2vw] mb-4 ${playfairDisplay.className}`}
+              >
+                Are not just meant to be watched—they’re meant to be lived
               </p>
-              <div className="border border-gray-500 relative   lg:h-[80vh] bg-black  flex flex-col items-center justify-center">
+              <div className="border border-gray-500 relative   lg:h-[70vh] bg-black  flex flex-col items-center justify-center">
                 <img
                   src="/assets/About-Section3-img.png"
                   alt="about image"
@@ -121,7 +219,7 @@ const AboutSecondSection = () => {
 
             <p
               data-aos="fade-left"
-              className={`mt-4 text-center lg:mt-0 w-full lg:w-5/12 text-[3.5vw] md:text-[4vw]   lg:text-[2.4vw] ${playfairDisplay.className}`}
+              className={` mt-4 text-center lg:mt-[20vh] w-full lg:w-4/12 text-[3.5vw] md:text-[4vw]   lg:text-[2.1vw] ${playfairDisplay.className}`}
             >
               At Locomotive, we elevate Middle Eastern cinema with authentic
               films that resonate deeply.
@@ -129,12 +227,11 @@ const AboutSecondSection = () => {
           </div>
         </div>
         <div className="pt-16 relative  bg-gradient-to-b   from-black     via-black  to-black    flex flex-col items-center w-full lg:py-8    px-8 lg:px-20 xl:px-24">
-          <div className="absolute top-0 left-0 w-full h-64 bg-gradient-to-b from-[#875d00] to-black/15 z-5 pointer-events-none" />
+          <div className="absolute top-0 left-0 w-full h-64  z-5 pointer-events-none" />
 
-          <div className="lg:mt-20 lg:h-[80vh] flex flex-col-reverse lg:flex-row items-center justify-between w-full">
+          <div className="lg:mt-20 lg:h-[80vh] flex flex-col-reverse lg:flex-row items-center justify-between w-full py-8">
             <p
-              data-aos="fade-right"
-              className={`mt-6  text-white text-center lg:mt-0 w-full lg:w-4/12 text-[3.5vw] md:text-[4vw] lg:text-[2.4vw] ${playfairDisplay.className}`}
+              className={`mt-2 md:mt-6  text-white text-center lg:mt-0 w-full lg:w-4/12 text-[3.5vw] md:text-[2vw] lg:text-[2.4vw] ${playfairDisplay.className}`}
             >
               Drawing from a diverse background and love for authentic
               storytelling.
@@ -165,57 +262,6 @@ const AboutSecondSection = () => {
               >
                 Our Founder
               </p> */}
-            </div>
-          </div>
-        </div>
-        <div className="  text-white text-center bg-black  pt-8  flex flex-col items-center w-full lg:py-8  py-4  px-8 lg:px-20 xl:px-24">
-          <div className=" lg:h-[100vh] flex flex-col lg:flex-row items-center justify-between w-full">
-            <div
-              className="flex flex-col-reverse lg:flex-col items-center w-full lg:w-6/12"
-              data-aos="fade-right"
-            >
-              <p
-                className={`mb-2 mt-4 lg:mt-0 text-[5vw] md:text-[6vw] lg:text-[2.3vw] ${playfairDisplay.className}`}
-              >
-                Executive producer
-              </p>
-              <div className="border border-gray-500 relative w-full lg:w-11/12  lg:h-[80vh]  bg-black  flex flex-col items-center justify-center">
-                <img
-                  src="/assets/About-Section5-img.png"
-                  alt="about image"
-                  className="w-full  h-full  object-cover transition-opacity opacity-100 duration-300    z-30"
-                />
-                <img
-                  src="/assets/About-Section5-img-bw.png"
-                  alt="about image"
-                  className="w-full  h-full  object-cover transition-opacity duration-300 absolute     hover:opacity-0  opacity-100 z-40"
-                />
-              </div>
-              {/* <img
-                src="/assets/About-Section5-img.png"
-                className="w-full lg:w-11/12  lg:h-[80vh] object-cover"
-                alt="about 3rd image"
-              /> */}
-            </div>
-            <div
-              className={`w-full lg:w-5/12 lg:h-[75vh] flex flex-col items-center justify-center`}
-              data-aos="fade-left"
-            >
-              <p
-                className={` lg:w-11/12 mb-8 ${playfairDisplay.className} text-[3.5vw] md:text-[4vw] lg:text-[1.2vw] text-center`}
-              >
-                Mariam Al Kaabi, our Executive Producer, plays a pivotal role in
-                bringing Locomotive’s vision to life. As a key investor, her
-                support and dedication to elevating Middle Eastern cinema have
-                been instrumental in our journey.
-              </p>
-              <p
-                className={` lg:w-11/12  ${playfairDisplay.className} text-[3.5vw] md:text-[4vw] lg:text-[1.2vw] text-center`}
-              >
-                Mariam’s belief in the power of storytelling and her commitment
-                to quality are integral to ensuring that every project we
-                undertake reaches its full potential.
-              </p>
             </div>
           </div>
         </div>
--- src/components/LandingAbout/LandingAbout.tsx
+++ src/components/LandingAbout/LandingAbout.tsx
@@ -1,27 +1,99 @@
+// "use client";
+// import { playfairDisplay } from "@/utils/fonts";
+// import React from "react";
+
+// const LandingAbout = () => {
+//   return (
+//     <div className=" p-20  text-white fixed   left-0 w-full sm:h-[100vh] h-[70vh] bg-gradient-to-b from-black via-black to-black   flex flex-col items-center justify-center   z-0  ">
+//       <div className="flex flex-col items-center justify-between w-full sm:pt-24 ">
+//         <div className="grid grid-cols-3 md:grid-cols-4   w-full sm:h-[100vh] h-[70vh]">
+//           <div className="relative pb-4 px-8 lg:px-16 xl:px-24 flex flex-col items-start justify-end w-full col-span-2 md:col-span-3 border-l-0 sm:h-[20vh] h-[20vh] border-gradient-l-to-r">
+//             <h1
+//               className={`font-mono text-[7vw] lg:text-[5vw] leading-[5vw] ${playfairDisplay.className} uppercase`}
+//             >
+//               ABOUT US
+//             </h1>
+//           </div>
+//           <div className="relative flex flex-col items-start w-full col-span-1  sm:h-[20vh] h-[20vh]  border-gradient-r-to-l border-r-0  border-white"></div>
+//           <div className="overflow-hidden p-2 md:px-4 lg:px-16  xl:px-24 flex flex-col md:flex-row items-center justify-center md:justify-between w-full  col-span-2 md:col-span-3 border-gradient-t-to-b  sm:h-[80vh] h-[50vh]">
+//             {/* <img
+//               className="w-10/12 md:w-4/12 h-[20vh] md:h-2/3 object-cover "
+//               src="/assets/About-landing-1.png"
+//             /> */}
+//             <div
+//               className="mr-10 relative w-10/12 md:w-5/12 h-[20vh] md:h-2/3    flex flex-col items-center justify-center"
+//               data-aos="fade-left"
+//             >
+//               <img
+//                 src="/assets/WhyUsHomeBg-clr.png"
+//                 alt="about image"
+//                 className="  w-full  h-full  object-cover transition-opacity opacity-100 duration-300    z-30"
+//               />
+//               <img
+//                 src="/assets/WhyUsHomeBg-bw.png"
+//                 alt="about image"
+//                 className=" w-full  h-full  object-cover transition-opacity duration-300 absolute     hover:opacity-0  opacity-100 z-40"
+//               />
+//             </div>
+//             <div
+//               className=" relative mt-4 md:mt-0 w-10/12 md:w-7/12 h-[20vh] md:h-2/3   bg-black  flex flex-col items-center justify-center"
+//               data-aos="fade-left"
+//             >
+//               <img
+//                 src="/assets/About-landing-2.png"
+//                 alt="about image"
+//                 className="w-full  h-full  object-cover transition-opacity opacity-100 duration-300    z-30"
+//               />
+//               <img
+//                 src="/assets/About-landing-2-bw.png"
+//                 alt="about image"
+//                 className="w-full  h-full  object-cover transition-opacity duration-300 absolute     hover:opacity-0  opacity-100 z-40"
+//               />
+//             </div>
+//           </div>
+//           <div className="ml-10 p-2 sm:px-4 flex flex-row items-center w-full col-span-1 border-gradient-t-to-b  sm:h-[80vh] h-[50vh]">
+//             <div className="flex flex-col items-start sm:justify-start  justify-center h-[50vh] sm:h-2/3 ">
+//               <h1
+//                 className={`w-1/2 sm:w-full font-normal text-[4vw] sm:text-[2vw] leading-[5vw] ${playfairDisplay.className}   text-[#EFA300]`}
+//               >
+//                 At Locomotive
+//               </h1>
+//               <p className="text-center font-light w-full text-[3vw] sm:text-[1.2vw] sm:w-8/12 leading-[5vw] sm:leading-[3vw]">
+//                 we believe that great stories have the power to move people and
+//                 connect Us all
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LandingAbout;
+
 "use client";
 import { playfairDisplay } from "@/utils/fonts";
 import React from "react";
 
 const LandingAbout = () => {
   return (
-    <div className=" p-20  text-white fixed   left-0 w-full sm:h-[100vh] h-[70vh] bg-gradient-to-b from-black via-black to-black   flex flex-col items-center justify-center   z-0  ">
+    <div
+      className={`py-20 sm:px-16  text-white fixed   left-0 w-full sm:h-[100vh] h-[70vh] bg-gradient-to-b from-black via-black to-black   flex flex-col items-center justify-center   z-0  ${playfairDisplay.className}`}
+    >
       <div className="flex flex-col items-center justify-between w-full sm:pt-24 ">
-        <div className="grid grid-cols-3 md:grid-cols-4   w-full sm:h-[100vh] h-[70vh]">
-          <div className="relative pb-4 px-8 lg:px-16 xl:px-24 flex flex-col items-start justify-end w-full col-span-2 md:col-span-3 border-l-0 sm:h-[20vh] h-[20vh] border-gradient-l-to-r">
+        <div className=" grid grid-cols-3 md:grid-cols-4   w-full sm:h-[100vh] h-[70vh]  ">
+          <div className="relative pb-4 px-8 lg:px-16 xl:px-10 flex flex-col items-start justify-end w-full col-span-2 md:col-span-3 border-l-0 border-r-0 sm:h-[20vh] h-[20vh] border-gradient-l-to-r">
             <h1
               className={`font-mono text-[7vw] lg:text-[5vw] leading-[5vw] ${playfairDisplay.className} uppercase`}
             >
               ABOUT US
             </h1>
           </div>
-          <div className="relative flex flex-col items-start w-full col-span-1  sm:h-[20vh] h-[20vh]  border-gradient-r-to-l border-r-0  border-white"></div>
-          <div className="overflow-hidden p-2 md:px-4 lg:px-16  xl:px-24 flex flex-col md:flex-row items-center justify-center md:justify-between w-full  col-span-2 md:col-span-3 border-gradient-t-to-b  sm:h-[80vh] h-[50vh]">
-            {/* <img
-              className="w-10/12 md:w-4/12 h-[20vh] md:h-2/3 object-cover "
-              src="/assets/About-landing-1.png"
-            /> */}
+          <div className="relative flex flex-col items-start w-full col-span-1  sm:h-[20vh] h-[20vh] border-l-0 border-gradient-r-to-l border-r-0  border-white"></div>
+          <div className="overflow-hidden p-2 md:px-4 lg:px-10  xl:px-10 flex flex-col md:flex-row items-center justify-center md:justify-between w-full  col-span-2 md:col-span-3   sm:h-[80vh] h-[50vh]">
             <div
-              className="mr-10 relative w-10/12 md:w-5/12 h-[20vh] md:h-2/3    flex flex-col items-center justify-center"
+              className=" sm:mr-10 relative w-full md:w-3/12 h-[20vh] md:h-2/3    flex flex-col items-center justify-center"
               data-aos="fade-left"
             >
               <img
@@ -36,7 +108,7 @@ const LandingAbout = () => {
               />
             </div>
             <div
-              className=" relative mt-4 md:mt-0 w-10/12 md:w-7/12 h-[20vh] md:h-2/3   bg-black  flex flex-col items-center justify-center"
+              className=" relative mt-4 md:mt-0 w-full md:w-9/12 h-[20vh] md:h-2/3   bg-black  flex flex-col items-center justify-center"
               data-aos="fade-left"
             >
               <img
@@ -50,23 +122,19 @@ const LandingAbout = () => {
                 className="w-full  h-full  object-cover transition-opacity duration-300 absolute     hover:opacity-0  opacity-100 z-40"
               />
             </div>
-            {/* <img
-              className="mt-4 md:mt-0 w-10/12 md:w-7/12 h-[20vh] md:h-2/3 object-cover "
-              src="/assets/About-landing-2.png"
-            /> */}
           </div>
-          <div className="ml-10 p-2 sm:px-4 flex flex-row items-center w-full col-span-1 border-gradient-t-to-b  sm:h-[80vh] h-[50vh]">
-            <div className="flex flex-col items-start sm:justify-start  justify-center h-[50vh] sm:h-2/3 ">
+          <div className="   sm:ml-10 md:ml-20 p-2 sm:px-4 flex flex-row items-center w-full col-span-1  relative sm:h-[80vh] h-[50vh]">
+            <div className="absolute bg-gradient-to-b  from-transparent via-white to-transparent bottom-[2vh] md:bottom-[15vh] right-[99%] sm:right-[110%]  w-[0.5px] sm:h-[80vh] h-[70vh]"></div>
+
+            <div className=" flex flex-col items-start sm:justify-start  justify-center h-[50vh] sm:h-2/3 ">
               <h1
-                className={`w-1/2 sm:w-full font-normal text-[4vw] sm:text-[2vw] leading-[5vw] ${playfairDisplay.className}   text-[#EFA300]`}
+                className={`sm:w-full sm:mr-4 text-center md:text-start font-bold text-[4.5vw] sm:text-[2vw] leading-[5vw] ${playfairDisplay.className}   text-[#EFA300]`}
               >
                 At Locomotive
               </h1>
-              <p className="text-center font-light w-full text-[3vw] sm:text-[1.2vw] sm:w-8/12 leading-[5vw] sm:leading-[3vw]">
-                We Believe That Great Stories Have The Power To Move People
-              </p>
-              <p className=" text-center font-light  w-full text-[3vw] sm:text-[1.2vw] sm:w-8/12 leading-[5vw] sm:leading-[3vw] mt-4">
-                Challenge Perspectives And Connect Us All
+              <p className="text-center font-light w-full text-[2.5vw] sm:text-[1.3vw] sm:w-9/12 leading-[5vw] sm:leading-[3.5vw]">
+                we believe that great stories have the power to move people and
+                connect Us all
               </p>
             </div>
           </div>
